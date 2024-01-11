e1b070ce1139af75cbd71f088ee1e060ab5b132d
itsSubroto <152801002+itsSubroto@users.noreply.github.com>
2024-01-11 18:20:24 +0000

    lecture 2

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,20 @@
-const
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountcity= "Jaipur"
+let accountState;
+
+// accountId=2 // not allowed
+accountEmail="hc@hc.com"
+accountPassword="2121212121"
+accountcity="Banaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity,accountState])
+
+/* 
+Prefer not to use var
+beacause of issue in block scope and functional scipe
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code newer version
+
+//alert( 3 + 3) // We are using nodejs, not browser
+console.log( 3 + 3)
+console.log("Subroto")
+
+let name= "subrata"
+let age= 18
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string =>" "
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof null);  // object data type
+console.log(typeof undefined); // undefined datatype
