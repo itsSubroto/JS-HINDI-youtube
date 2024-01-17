1185c66b7fe6e4cdc68b83d4256f83202b50e797
itsSubroto <subratasantra9638@gmail.com>
2024-01-17 23:33:49 +0530

    2nd commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,3 +2,8 @@ console.log("Subroto")
 console.log("Marokhana")
 console.log("Khanakul")
 console.log("Hooghly")
+
+console.log("Pritam")
+console.log("Sundorpur")
+console.log("Arambagh")
+console.log("Kolkata")
